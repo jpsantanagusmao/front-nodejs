5eac9cb1602d44adeef190042f53838da0dcbb1c
João Paulo Santana Gusmão <teo.itinga@gmail.com>
2022-03-22 23:54:34 -0300

    updates

--- ares/src/app/shared/components/dap-mda/dap-web/dap-web.component.ts
+++ ares/src/app/shared/components/dap-mda/dap-web/dap-web.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap, delay } from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Component({
   selector: 'dap-web',
@@ -20,6 +21,30 @@ export class DapWebComponent implements OnInit {
   dap$: Observable<any>;
   ater: any;
 
+  /**
+   * 
+   */
+  pronome_tratamento_fem: string[] = ['Sra. ', 'Dra. ', 'Sua '];
+  pronome_tratamento_masc: string[] = ['Sr. ', 'Seu '];
+
+  recomendacoesVector: string[] = [
+    '- Providenciar os documentos solicitados e encaminhar ao escritório local para que possa ser realizado o seu registro. ',
+    `A DAP é um documento de direito do Agricultor Familiar e a emissão da mesma é GRATUITA. Como é um documento federal, qualquer ação judicial será desferida na esfera federal (Polícia Federal).`
+  ]
+
+  orientacoesVector: string[] = [
+    `A DAP - Declaração de Aptidão do PRONAF é utilizada como instrumento de identificação do agricultor familiar para acessar políticas públicas do Governo Federal, e é necessário apresentar no ato da elaboração da DAP os seguintes documentos: 
+     CPF - do agricultor (a) familiar e cônjuge;
+ Identidade (RG) do agricultor (a) familiar e cônjuge;
+ Certidão de casamento;
+ Comprovante de residência (conta de energia);
+ Contrato de Compra e Venda, Declaração de Posse ou Matrícula do Cartório de
+Registro de Imóveis dos estabelecimentos rurais beneficiados com o crédito;
+ Comprovante de renda dos últimos 12 meses, tais como: notas fiscais de entrada;
+holerite do agricultor (a), cônjuge e agregados (se possuir); recibos;
+    `
+  ];
+
   constructor(
     private _dapService: DapService
   ) { }
@@ -29,7 +54,7 @@ export class DapWebComponent implements OnInit {
       search: new FormControl('', [Validators.required, Validators.minLength(11), Validators.maxLength(11)])
     });
   }
- 
+
   async emitReport() {
     /**
  * Consulta irregularidades
@@ -40,20 +65,63 @@ export class DapWebComponent implements OnInit {
     await this.loadingToggle();
 
     this.dap$ = this._dapService.findByCpf(cpf).pipe(
-      tap((d)=>{
+      tap((d) => {
         obj.loadingToggle()
       })
     );
-    
+
   }
- 
+
   async loadingToggle() {
     this._loading = !(Boolean(this._loading));
   }
 
-  registerTreatment(dap) {
+  async registerTreatment(dap) {
     console.log(dap);
+    let situacao = '';
+    //const pronome = dap.titular.genero == 'Masculino' ? this.pronome_tratamento_masc : this.pronome_tratamento_fem;
+
+    let pronome = '';
+
+    if(dap.titular.genero == 'Masculino'){
+      pronome = this.pronome_tratamento_masc[Math.floor(Math.random() * this.pronome_tratamento_masc.length)]
+    }else{
+      pronome = this.pronome_tratamento_fem[Math.floor(Math.random() * this.pronome_tratamento_fem.length)]
+
+    }
+
+    let producao_texto = '';
+    const qtd = dap.rendas.length;
+    let i: number = 1;
+
+    const dapVencida = await moment().isAfter(moment(dap.validade));
+
+    await dap.rendas.map(r => {
+      if (i == qtd) {
+        producao_texto = producao_texto + `${r.producao} `;
+      } else {
+        if (i == (qtd - 1)) {
+          producao_texto = producao_texto + `, ${r.producao} e `;
+        } else {
+          producao_texto = producao_texto + `${r.producao}`;
+        }
+      }
+      i = i + 1;
+    });
+
+    console.log('dapVencida');
+    console.log(dapVencida);
+    if(dapVencida){
+      situacao = `O ${pronome} ${dap.titular.nome}, proprietario de uma área total de ${dap.areaDaPropriedade.toFixed(2)} hectares no local denominado "${dap.nomeImovelPrincipal}" possui a DAP de número ${dap.numDap} com validade até ${moment(dap.validade).format('DD/MM/yyyy')} na qual consta a produção de ${producao_texto}.`;
+    }else{
+      situacao = `O ${pronome} ${dap.titular.nome}, proprietario de uma área total de ${dap.areaDaPropriedade.tofixed(2)} hectares no local denominado "${dap.nomeImovelPrincipal}" possui a DAP de número ${dap.numDap} que expirou na data de ${moment(dap.validade).format('DD/MM/yyyy')}. Neste documento consta a produção de ${producao_texto}.`;
+    }
+
+    let orientacao = moment().isBefore(moment(dap.validade)) ? this.orientacoesVector[Math.floor(Math.random() * this.orientacoesVector.length)] : `Como a DAP se econtra EXPIRADA é necessário providenciar os documentos para a renovação.`;
+    //let recomendacao = recomendacoes[Math.random(this.recomendacoesVector.length)];
+    let recomendacao = this.recomendacoesVector[Math.floor(Math.random() * this.recomendacoesVector.length)];
+
+    this.onReport.emit({ situacao, orientacao, recomendacao })
   }
 }
 
- 
