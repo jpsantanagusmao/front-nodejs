2e293505a615a73f62ba2eb9d4a61b4abce81ea3
teoitinga <teo.itinga@gmail.com>
2022-04-19 18:12:36 -0300

    updates

--- ares/src/app/shared/components/dialog-rater/dialog-rater.component.ts
+++ ares/src/app/shared/components/dialog-rater/dialog-rater.component.ts
@@ -12,14 +12,14 @@ export class DialogRaterComponent implements OnInit {
 
   @Input() title: string = 'Relatório de Assist. Técnica';
   @Input() confirmMessage: string;
-  
+
   @Input() confirmText: string = 'Cofirmar';
   @Input() cancelTxt: string = 'Cancelar';
-  
+
   @Input() situacaoTxt: string;
   @Input() orientacaoTxt: string;
   @Input() recomendacaoTxt: string;
-  
+
   confirmResult: Subject<boolean>;
 
   form: FormGroup;
@@ -27,19 +27,19 @@ export class DialogRaterComponent implements OnInit {
   constructor(public bsModalRef: BsModalRef) {
 
   }
-  
-  async _getRater(){
+
+  async _getRater() {
     const s = await this.situacaoTxt;
     const o = await this.orientacaoTxt;
     const r = await this.recomendacaoTxt;
-    
-    return {situacao: s, orientacao: o, recomendacao: r};
+
+    return { situacao: s, orientacao: o, recomendacao: r };
   }
   ngOnInit(): void {
     this.confirmResult = new Subject();
     this._createForm();
   }
-  async _createForm(){
+  async _createForm() {
     const obj = this;
 
     this.form = new FormGroup({
@@ -49,12 +49,35 @@ export class DialogRaterComponent implements OnInit {
     });
     this.setForm();
   }
-  setForm(){
+  setForm() {
     this.form.controls.situacao.patchValue(this.situacaoTxt);
     this.form.controls.orientacao.patchValue(this.orientacaoTxt);
     this.form.controls.recomendacao.patchValue(this.recomendacaoTxt);
   }
-  onConfirm(){
+  onCopy() {
+
+    const rater = {
+      situacao: this.form.controls.situacao.value,
+      orientacao: this.form.controls.orientacao.value,
+      recomendacao: this.form.controls.recomendacao.value,
+    }
+
+    const ratertxt = `Situação atual: 
+${rater.situacao}
+Orientações:
+${rater.orientacao}
+
+Recomendações:
+${rater.recomendacao}`;
+
+    navigator.clipboard.writeText(ratertxt).then(
+      (txt) => {
+        alert('O conteúdo foi copiado, agora é só utilizar ctrl+v e colar no seu formulário.')
+        this._confirmAndClose(rater);
+      }
+    );
+  }
+  onConfirm() {
     const rater = {
       situacao: this.form.controls.situacao.value,
       orientacao: this.form.controls.orientacao.value,
@@ -62,21 +85,21 @@ export class DialogRaterComponent implements OnInit {
     }
     this._confirmAndClose(rater);
   }
-  formValid(){
-    if(
+  formValid() {
+    if (
       (this.form.controls.situacao.dirty)
       && (this.form.controls.orientacao.dirty)
       && (this.form.controls.recomendacao.dirty)
-      ){
+    ) {
       return true;
     }
     return false;
   }
-  onClose(){
+  onClose() {
     this._confirmAndClose(false);
   }
 
-  private _confirmAndClose(value: any){
+  private _confirmAndClose(value: any) {
     this.confirmResult.next(value);
     this.bsModalRef.hide();
 
