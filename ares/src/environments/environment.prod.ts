2d4e7a26e9768c2ba9c7929aa374cf851399bbbf
João Paulo Santana Gusmão <teo.itinga@gmail.com>
2022-12-02 19:02:32 -0300

    Update environment.prod.ts

--- ares/src/environments/environment.prod.ts
+++ ares/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  BASE_API_URL: 'http://localhost:13000/api/v1/',
-  //BASE_API_URL: 'http://45.174.40.217:13000/api/v1/',
+  //BASE_API_URL: 'http://localhost:13000/api/v1/',
+  BASE_API_URL: 'http://45.174.40.217:1355/api/v1/',
 };
