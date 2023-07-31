5ddcf36e84a9a96f54568b141ce6d5e2c6cc4953
João Paulo Santana Gusmão <52930272+teoitinga@users.noreply.github.com>
2023-07-31 15:53:04 -0300

    atualizações página contato...ok...por enquanto
    Ajuste na página de contatos. Ainda ão dados estáticos, falta criar o serviço de busca na API.

--- ares/src/app/modules/acomp/acomp.module.ts
+++ ares/src/app/modules/acomp/acomp.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { CommonModule, registerLocaleData } from '@angular/common';
 
 import { AcompRoutingModule } from './acomp-routing.module';
 import { TelaAcompComponent } from './components/tela-acomp/tela-acomp.component';
 import { PropriedadeRuralComponent } from './components/propriedade-rural/propriedade-rural.component';
 import { ContatoFuncionarioComponent } from './components/contato-funcionario/contato-funcionario.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import localePT from '@angular/common/locales/pt';
+
+registerLocaleData(localePT);
 
 @NgModule({
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-br' },
+   ],
   declarations: [TelaAcompComponent, PropriedadeRuralComponent, ContatoFuncionarioComponent],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     AcompRoutingModule
   ]
 })
--- ares/src/app/modules/acomp/components/contato-funcionario/contato-funcionario.component.ts
+++ ares/src/app/modules/acomp/components/contato-funcionario/contato-funcionario.component.ts
@@ -14,6 +14,16 @@ export class ContatoFuncionarioComponent implements OnInit {
   colabs = COLABORARORES;
   info: colaborador;
   expressaozap: string;
+  isopen:boolean = true;
+
+
+  abre = moment().hour(7).minute(30);
+  almoco_sai = moment().hour(11).minute(30);
+  almoco_ret = moment().hour(13).minute(30);
+  fecha = moment().hour(17).minute(30);
+  dia_semana_comeca = moment().day(1);// Segunda-feira
+  dia_semana_termina = moment().day(5);// Sexta-feira
+
 
   constructor(
     private _route: ActivatedRoute,
@@ -27,7 +37,30 @@ export class ContatoFuncionarioComponent implements OnInit {
       // atual tiver propriedade `id` igual a 2.
       return obj.id === id
     })
-    this._criaExpressao()
+    
+    if(this._isopen()){
+      this._criaExpressao()
+    }
+
+  }
+
+  private _isopen() {
+
+    if( ( moment().isAfter(this.abre) //Para estar aberto a hora atual deve ser após de 7:30
+        && moment().isBefore(this.almoco_sai) ) //Para estar aberto a hora atual deve antes de 11:30
+        || ( (moment().isAfter(this.almoco_ret) //Para estar aberto a hora atual deve ser após 13:30
+        && moment().isBefore(this.fecha) ) ) //Para estar aberto a hora atual deve ser antes de 17:30
+        && (moment().isAfter(this.dia_semana_comeca) //Para estar aberto o dia da semana deve de segunda-feira(1) em diante
+        && moment().isBefore(this.dia_semana_termina)) //Para estar aberto o dia da semana deve ser de sexta-feira(5) para trás
+    ){
+      this.isopen=true;
+    }else{
+      this.isopen=false;
+    }
+    //this.isopen=false;
+    
+    return this.isopen;
+
   }
 
   private _criaExpressao(){
@@ -43,7 +76,6 @@ export class ContatoFuncionarioComponent implements OnInit {
 
     let msg = `https://wa.me/55${fone}?text=${comp}preciso%20da%20sua%20orientação.`
     this.expressaozap = msg;
-    console.log(this.expressaozap);
 
   }
 }
